73317091e5baedf8bfe9524b4d1738e73b9ba4f2
ZYC <2095921712@qq.com>
2022-05-30 20:38:28 +0800

    feat:今日已完成

--- src/views/attendances/attendances.vue
+++ src/views/attendances/attendances.vue
@@ -3,8 +3,25 @@
     <div class="app-container">
       <el-card>
         <!-- 具体页面结构 -->
-        组织架构
+        考勤管理
+        <!-- <son @aaa="num" /> -->
+        <son v-model="num" />
       </el-card>
     </div>
   </div>
 </template>
+
+<script>
+import son from './son.vue'
+export default {
+  components: {
+    son
+  },
+  data() {
+    return {
+      num: 0
+    }
+  }
+
+}
+</script>
---
+++ src/views/attendances/son.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    数字:{{ value }}
+    <button @click="btn">+1</button>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: {
+    value: { type: Number, required: true }
+  },
+
+  methods: {
+    btn() {
+      this.$emit('input', this.value + 1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
